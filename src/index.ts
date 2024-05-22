ce975b5314503bc7fe8938e49b7510e197998157
taghassan541 <taghassan55@gmail.com>
2024-05-22 14:42:25 +0400

    export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import {useStrapi} from "./runtime/useStrapi";
 import {strapiConfig} from "./runtime/config/strapiConfig"
+
+import {useStrapiAuth} from "./runtime/composables/useStrapiAuth";
+import {useStrapiUser} from "./runtime/composables/useStrapiUser";
+import {useStrapiUrl} from "./runtime/composables/useStrapiUrl";
+import {useStrapiVersion} from "./runtime/composables/useStrapiVersion";
+import {useStrapiToken} from "./runtime/composables/useStrapiToken";
+import {useStrapiClient} from "./runtime/composables/useStrapiClient";
+import {useStrapiGraphQL} from "./runtime/composables/useStrapiGraphQL";
+import {useStrapiMedia} from "./runtime/composables/useStrapiMedia";
+
+
 /*
 * ufo
 * axios
@@ -10,6 +21,14 @@ export * from "./runtime/types"
 export * from "./runtime/composables";
 export {
     useStrapi,
-    strapiConfig
+    strapiConfig,
+    useStrapiAuth,
+    useStrapiUser,
+    useStrapiUrl,
+    useStrapiVersion,
+    useStrapiToken,
+    useStrapiClient,
+    useStrapiGraphQL,
+    useStrapiMedia
 }
 export default useStrapi
