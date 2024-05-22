187dae7ad6c3083f3aa32e0e1210e78ac673380e
taghassan541 <taghassan55@gmail.com>
2024-05-22 14:16:40 +0400

    c

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
 import {useStrapi} from "./runtime/useStrapi";
-// import {useStrapiVersion} from "./runtime/composables/useStrapiVersion";
-// import {strapiConfig} from "./runtime/config/strapiConfig";
-// import {useStrapiUrl} from "./runtime/composables/useStrapiUrl";
-// import {useStrapiToken} from "./runtime/composables/useStrapiToken";
-// import {useStrapiClient} from "./runtime/composables/useStrapiClient";
-// import {useStrapiUser} from "./runtime/composables/useStrapiUser";
-// import {useStrapiAuth} from "./runtime/composables/useStrapiAuth";
-// import {useStrapiGraphQL} from "./runtime/composables/useStrapiGraphQL";
-// import {useStrapiMedia} from "./runtime/composables/useStrapiMedia";
 
 /*
 * ufo
@@ -15,19 +6,3 @@ import {useStrapi} from "./runtime/useStrapi";
 * qs
 *  */
 export {useStrapi} from './runtime/useStrapi'
-// export * from './runtime/types'
-export * from './runtime/composables'
-
-// export {
-//     strapiConfig,
-//     useStrapiAuth,
-//     useStrapiUser,
-//     useStrapiUrl,
-//     useStrapiVersion,
-//     useStrapi,
-//     useStrapiToken,
-//     useStrapiClient,
-//     useStrapiGraphQL,
-//     useStrapiMedia
-// }
-
