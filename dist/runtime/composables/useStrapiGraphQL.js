efe33bac03759807a6512c039acf88f69b1746b8
taghassan541 <taghassan55@gmail.com>
2024-05-23 07:55:21 +0400

    remove dist

--- dist/runtime/composables/useStrapiGraphQL.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useStrapiGraphQL = void 0;
-const useStrapiGraphQL = () => { };
-exports.useStrapiGraphQL = useStrapiGraphQL;
--- dist/runtime/useStrapi.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useStrapi = void 0;
-const useStrapi4_1 = require("./useStrapi4");
-const useStrapi = () => {
-    return (0, useStrapi4_1.useStrapi4)();
-};
-exports.useStrapi = useStrapi;
