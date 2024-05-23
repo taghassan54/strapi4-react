931bcfc90f7b8b275cacbd9ebac7c00c85a1cf54
taghassan541 <taghassan55@gmail.com>
2024-05-23 11:27:22 +0400

    defaultErrors

--- dist/runtime/composables/useStrapiClient.js
+++ dist/runtime/composables/useStrapiClient.js
@@ -8,50 +8,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.useStrapiClient = void 0;
+const axios_1 = require("axios");
 const qs_1 = require("qs");
+const axios_2 = __importDefault(require("axios"));
 const useStrapiUrl_1 = require("./useStrapiUrl");
 const useStrapiToken_1 = require("./useStrapiToken");
-const defaultErrors = (err) => ({
-    v4: {
-        error: {
-            status: 500,
-            name: 'UnknownError',
-            message: err.message,
-            details: err
-        }
-    },
-    v3: {
-        error: 'UnknownError',
-        message: err.message,
-        statusCode: 500
-    }
-});
+const useStrapiVersion_1 = require("./useStrapiVersion");
 const useStrapiClient = () => {
     const { userUrl, adminUrl } = (0, useStrapiUrl_1.useStrapiUrl)();
     const { getToken } = (0, useStrapiToken_1.useStrapiToken)();
+    const version = (0, useStrapiVersion_1.useStrapiVersion)();
+    const defaultErrors = (err) => ({
+        v4: {
+            error: {
+                status: 500,
+                name: 'UnknownError',
+                message: err.message,
+                details: err
+            }
+        },
+        v3: {
+            error: 'UnknownError',
+            message: err.message,
+            statusCode: 500
+        }
+    });
     return (url_1, ...args_1) => __awaiter(void 0, [url_1, ...args_1], void 0, function* (url, fetchOptions = {}, isForAdmin = false) {
         try {
+            const headers = new axios_1.AxiosHeaders();
+            headers.setContentType('application/json');
+            headers.setAccept('application/json');
             const token = getToken();
-            // const headers: AxiosHeaders = new AxiosHeaders()
-            //
-            // headers.setContentType('application/json')
-            // headers.setAccept('application/json')
-            //
-            //
-            // if (token != null) {
-            //     headers.Authorization = `Bearer ${token}`
-            // }
-            //
-            // if (token == null) {
-            //     headers.setAuthorization(null)
-            // }
-            const headers = {
-                'accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': token != null ? `Bearer ${adminUrl}` : null,
-            };
+            if (token != null) {
+                headers.Authorization = `Bearer ${token}`;
+            }
+            if (token == null) {
+                headers.setAuthorization(null);
+            }
             // Map params according to strapi v3 and v4 formats
             if (fetchOptions.params) {
                 const params = (0, qs_1.stringify)(fetchOptions.params, { encodeValuesOnly: true });
@@ -61,34 +59,22 @@ const useStrapiClient = () => {
                 delete fetchOptions.params;
             }
             let config = Object.assign({ maxBodyLength: Infinity, url: `${isForAdmin ? adminUrl() : userUrl()}/${url}`, headers: headers, body: JSON.stringify(fetchOptions.data) }, fetchOptions);
-            const options = {};
-            Object.entries(config).forEach(([key, value]) => {
-                options[key] = value;
-            });
-            try {
-                // @ts-expect-error method is not explicitly typed
-                return yield fetch(url, config);
+            // const response1 = await fetch(`${isForAdmin ? adminUrl() : userUrl()}/${url}`,config)
+            const response = yield axios_2.default.request(config);
+            return response.data;
+        }
+        catch (err) {
+            if (err instanceof axios_1.AxiosError) {
+                const e = err.data || defaultErrors(err)[version];
+                // nuxt.hooks.callHook('strapi:error', e)
+                console.log('====================================');
+                console.log("client Error ", e);
+                console.log('====================================');
+                throw e;
             }
-            catch (err) {
-                // const e: Strapi4Error | Strapi3Error = err.data || defaultErrors(err)['v4']
+            else {
                 throw err;
             }
-            // // const response = await axios(config)
-            // // const response = await axios.request(config)
-            //
-            // const response =fetch(`${isForAdmin ? adminUrl() : userUrl()}/${url}`,options)
-            //
-            // // return response.data
-            //
-            // return  await ((await response).json())
-        }
-        catch (e) {
-            // const e: Strapi4Error | Strapi3Error = err.data || defaultErrors(err)[version]
-            // nuxt.hooks.callHook('strapi:error', e)
-            console.log('====================================');
-            console.log("client Error ", e);
-            console.log('====================================');
-            throw e;
         }
     });
 };
--- src/runtime/composables/useStrapiClient.ts
+++ src/runtime/composables/useStrapiClient.ts
@@ -1,60 +1,51 @@
-import {AxiosHeaders, AxiosRequestConfig} from "axios";
-
-import type { FetchError, FetchOptions } from 'ofetch'
-
+import {AxiosError, AxiosHeaders, AxiosRequestConfig} from "axios";
 
 import {stringify} from 'qs'
 import axios from 'axios';
 import {useStrapiUrl} from "./useStrapiUrl";
 import {useStrapiToken} from "./useStrapiToken";
+import {useStrapiVersion} from "./useStrapiVersion";
 import {Strapi3Error, Strapi4Error} from "../types";
 
-const defaultErrors = (err: FetchError) => ({
-    v4: {
-        error: {
-            status: 500,
-            name: 'UnknownError',
-            message: err.message,
-            details: err
-        }
-    },
-    v3: {
-        error: 'UnknownError',
-        message: err.message,
-        statusCode: 500
-    }
-})
 
 export const useStrapiClient = () => {
 
     const {userUrl, adminUrl} = useStrapiUrl()
     const {getToken} = useStrapiToken()
+    const version = useStrapiVersion()
+
+    const defaultErrors = (err: AxiosError) => ({
+        v4: {
+            error: {
+                status: 500,
+                name: 'UnknownError',
+                message: err.message,
+                details: err
+            }
+        },
+        v3: {
+            error: 'UnknownError',
+            message: err.message,
+            statusCode: 500
+        }
+    })
 
     return async <T>(url: string, fetchOptions: AxiosRequestConfig = {}, isForAdmin = false): Promise<T> => {
 
         try {
 
+            const headers: AxiosHeaders = new AxiosHeaders()
+
+            headers.setContentType('application/json')
+            headers.setAccept('application/json')
             const token = getToken()
 
+            if (token != null) {
+                headers.Authorization = `Bearer ${token}`
+            }
 
-            // const headers: AxiosHeaders = new AxiosHeaders()
-            //
-            // headers.setContentType('application/json')
-            // headers.setAccept('application/json')
-            //
-            //
-            // if (token != null) {
-            //     headers.Authorization = `Bearer ${token}`
-            // }
-            //
-            // if (token == null) {
-            //     headers.setAuthorization(null)
-            // }
-
-            const headers={
-                'accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization':token!=null? `Bearer ${adminUrl}`:null,
+            if (token == null) {
+                headers.setAuthorization(null)
             }
 
             // Map params according to strapi v3 and v4 formats
@@ -67,45 +58,32 @@ export const useStrapiClient = () => {
             }
 
 
-
             let config = {
                 maxBodyLength: Infinity,
                 url: `${isForAdmin ? adminUrl() : userUrl()}/${url}`,
                 headers: headers,
-                body :JSON.stringify(fetchOptions.data),
+                body:JSON.stringify(fetchOptions.data),
                 ...fetchOptions
             };
-            const options:any={}
-            Object.entries(config).forEach(([key, value]) => {
-                options[key] = value
-            })
 
-            try {
-                // @ts-expect-error method is not explicitly typed
-                return await fetch<T>(url, config)
-            } catch (err) {
-                // const e: Strapi4Error | Strapi3Error = err.data || defaultErrors(err)['v4']
+            // const response1 = await fetch(`${isForAdmin ? adminUrl() : userUrl()}/${url}`,config)
+            const response = await axios.request(config)
 
+            return response.data
+
+        } catch (err) {
+            if(err instanceof AxiosError) {
+
+                const e: Strapi4Error | Strapi3Error = err.data || defaultErrors(err)[version]
+
+                // nuxt.hooks.callHook('strapi:error', e)
+                console.log('====================================');
+                console.log("client Error ", e);
+                console.log('====================================');
+                throw e
+            }else{
                 throw err
             }
-
-            // // const response = await axios(config)
-            // // const response = await axios.request(config)
-            //
-            // const response =fetch(`${isForAdmin ? adminUrl() : userUrl()}/${url}`,options)
-            //
-            // // return response.data
-            //
-            // return  await ((await response).json())
-
-        } catch (e) {
-            // const e: Strapi4Error | Strapi3Error = err.data || defaultErrors(err)[version]
-
-            // nuxt.hooks.callHook('strapi:error', e)
-            console.log('====================================');
-            console.log("client Error ", e);
-            console.log('====================================');
-            throw e
         }
     }
 
